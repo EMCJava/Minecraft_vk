d98e71670fde7ea228d4ece368187bbf81e06a21
EMCJava <loyusum246807952@gmail.com>
2022-06-01 22:19:54 +0800

    Temp chunk pool test code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <Include/GlobalConfig.hpp>
 #include <Minecraft/Application/MainApplication.hpp>
+#include <Minecraft/World/Chunk/ChunkPool.hpp>
+#include <Minecraft/World/MinecraftWorld.hpp>
 
 #include <iostream>
 
@@ -11,6 +13,36 @@ main( )
         bool result = GlobalConfig::LoadFromFile( "../../config.ini" );
         assert( result );
     }
+
+/*    ChunkPool pool( 16 );
+    pool.SetCentre( { 0, 10, 10 } );
+
+    for ( int i = 0; i < 1; ++i )
+        for ( int j = 0; j < 20; ++j )
+            for ( int k = 0; k < 20; ++k )
+                pool.AddCoordinate( { i, j, k } );
+
+    pool.StartThread( );
+    while ( pool.PoolSize( ) > 300 )
+    {
+        for ( int i = 0; i < 1; ++i )
+        {
+            for ( int j = 0; j < 20; ++j )
+            {
+                for ( int k = 0; k < 20; ++k )
+                {
+                    std::cout << (pool.IsChunkLoaded( { i, j, k } ) ? "@ " : pool.IsChunkLoading( { i, j, k } ) ? "* " : "- ");
+                }
+                std::cout << std::endl;
+            }
+
+            std::cout << std::endl;
+        }
+
+        std::this_thread::sleep_for( std::chrono::milliseconds( 200 ) );
+    }
+
+    return 0;*/
 
     bool success = true;
 
