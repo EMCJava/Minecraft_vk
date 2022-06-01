21fa8a75e191f7cec74159840ab5b12c29048c17
EMCJava <loyusum246807952@gmail.com>
2022-06-01 22:19:37 +0800

    Added block class

---
+++ Minecraft/Block/Block.cpp
@@ -0,0 +1,5 @@
+//
+// Created by loys on 5/29/22.
+//
+
+#include "Block.hpp"
---
+++ Minecraft/Block/Block.hpp
@@ -0,0 +1,14 @@
+//
+// Created by loys on 5/29/22.
+//
+
+#ifndef MINECRAFT_VK_BLOCK_HPP
+#define MINECRAFT_VK_BLOCK_HPP
+
+
+class Block
+{
+};
+
+
+#endif   // MINECRAFT_VK_BLOCK_HPP
