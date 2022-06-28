291901e7ce339c6f50bd1ef4f2715c889eca18bb
EMCJava <loyusum246807952@gmail.com>
2022-06-28 13:45:25 +0800

    Added block id

--- Minecraft/Block/Block.hpp
+++ Minecraft/Block/Block.hpp
@@ -5,9 +5,16 @@
 #ifndef MINECRAFT_VK_BLOCK_HPP
 #define MINECRAFT_VK_BLOCK_HPP
 
+enum BlockID {
+    Air, Stone, Grass, BlockIDSize
+};
 
 class Block
 {
+    BlockID id;
+
+public:
+    Block(BlockID id = Air) : id(id){}
 };
 
 
