6621db5789e482df2bd11f9a06c3a16aa64a8c18
EMCJava <loyusum246807952@gmail.com>
2022-06-30 22:44:55 +0800

    Render whole chunk instead of surface height

--- Minecraft/World/Chunk/ChunkCache.cpp
+++ Minecraft/World/Chunk/ChunkCache.cpp
@@ -18,49 +18,110 @@ ChunkCache::ResetLoad( )
 
     auto [ chunkX, chunkZ, chunkY ] = chunk.GetCoordinate( );
 
-    chunkX <<= SectionBinaryOffsetLength;
-    chunkZ <<= SectionBinaryOffsetLength;
+    chunkX <<= SectionUnitLengthBinaryOffset;
+    chunkZ <<= SectionUnitLengthBinaryOffset;
 
     chunk.RegenerateChunk( );
+    if ( chunk.m_VisibleFacesCount == 0 ) return;
 
-    static const std::vector<uint16_t>   blockIndices  = { 0, 1, 2, 2, 3, 0 };
-    std::vector<DataType::ColoredVertex> BlockVertices = {
-        {{ -0.5f, 0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
-        { { 0.5f, 0.0f, -0.5f }, { 0.0f, 0.0f, 1.0f }},
-        {  { 0.5f, 0.0f, 0.5f }, { 0.0f, 1.0f, 0.0f }},
-        { { -0.5f, 0.0f, 0.5f }, { 1.0f, 1.0f, 1.0f }}
+    //      F----H
+    //  E---|G   |
+    //  |  B-|--D
+    //  A----C
+
+    static constexpr auto FaceVerticesCount = 4;
+    static constexpr auto FaceIndicesCount  = 6;
+
+
+    static const std::array<uint16_t, FaceIndicesCount> blockIndices = { 0, 1, 2, 2, 3, 0 };
+
+    static const std::array<DataType::ColoredVertex, FaceVerticesCount> FrontBlockVertices = {
+        DataType::ColoredVertex {{ 1.f, 0.0f, 0.f }, { 1.0f, 0.f, 0.f  }}, // B
+        DataType::ColoredVertex { { 1.f, 1.f, 0.f }, { 1.0f, 0.f, 0.f  }}, // F
+        DataType::ColoredVertex { { 1.f, 1.f, 1.f }, { 1.0f, 0.f, 0.f  }}, // H
+        DataType::ColoredVertex {{ 1.f, 0.0f, 1.f }, { 1.0f, 0.f, 0.f  }}, // D
     };
-    const auto  verticesDataSize = sizeof( DataType::ColoredVertex ) * BlockVertices.size( ) << ( SectionBinaryOffsetLength * 2 );
-    const auto  indicesDataSize  = sizeof( uint16_t ) * blockIndices.size( ) << ( SectionBinaryOffsetLength * 2 );
+
+    static const std::array<DataType::ColoredVertex, FaceVerticesCount> BackBlockVertices = {
+        DataType::ColoredVertex {{ 0.f, 0.0f, 0.f }, { 1.0f, 0.f, 0.f }}, // A
+        DataType::ColoredVertex {{ 0.f, 0.0f, 1.f }, { 1.0f, 0.f, 0.f }}, // C
+        DataType::ColoredVertex { { 0.f, 1.f, 1.f }, { 1.0f, 0.f, 0.f }}, // G
+        DataType::ColoredVertex { { 0.f, 1.f, 0.f }, { 1.0f, 0.f, 0.f }}, // E
+    };
+
+    static const std::array<DataType::ColoredVertex, FaceVerticesCount> RightBlockVertices = {
+        DataType::ColoredVertex {{ 0.f, 0.0f, 1.f }, { 0.f, 0.f, 1.f }}, // C
+        DataType::ColoredVertex {{ 1.f, 0.0f, 1.f }, { 0.f, 0.f, 1.f }}, // D
+        DataType::ColoredVertex { { 1.f, 1.f, 1.f }, { 0.f, 0.f, 1.f }}, // H
+        DataType::ColoredVertex { { 0.f, 1.f, 1.f }, { 0.f, 0.f, 1.f }}, // G
+    };
+
+    static const std::array<DataType::ColoredVertex, FaceVerticesCount> LeftBlockVertices = {
+        DataType::ColoredVertex {{ 0.f, 0.0f, 0.f }, { 0.f, 0.f, 1.f }}, // A
+        DataType::ColoredVertex { { 0.f, 1.f, 0.f }, { 0.f, 0.f, 1.f }}, // E
+        DataType::ColoredVertex { { 1.f, 1.f, 0.f }, { 0.f, 0.f, 1.f }}, // F
+        DataType::ColoredVertex {{ 1.f, 0.0f, 0.f }, { 0.f, 0.f, 1.f }}, // B
+    };
+
+    static const std::array<DataType::ColoredVertex, FaceVerticesCount> UpBlockVertices = {
+        DataType::ColoredVertex {{ 0.f, 1.f, 0.f }, { 0.f, 1.f, 0.f }}, // E
+        DataType::ColoredVertex {{ 0.f, 1.f, 1.f }, { 0.f, 1.f, 0.f }}, // G
+        DataType::ColoredVertex {{ 1.f, 1.f, 1.f }, { 0.f, 1.f, 0.f }}, // H
+        DataType::ColoredVertex {{ 1.f, 1.f, 0.f }, { 0.f, 1.f, 0.f }}, // F
+    };
+
+    static const std::array<DataType::ColoredVertex, FaceVerticesCount> DownBlockVertices = {
+        DataType::ColoredVertex {{ 0.f, 0.0f, 0.f }, { 0.f, 1.f, 0.f }}, // A
+        DataType::ColoredVertex {{ 1.f, 0.0f, 0.f }, { 0.f, 1.f, 0.f }}, // B
+        DataType::ColoredVertex {{ 1.f, 0.0f, 1.f }, { 0.f, 1.f, 0.f }}, // D
+        DataType::ColoredVertex {{ 0.f, 0.0f, 1.f }, { 0.f, 1.f, 0.f }}, // C
+    };
+
+    const auto  verticesDataSize = sizeof( DataType::ColoredVertex ) * FaceVerticesCount * chunk.m_VisibleFacesCount;
+    const auto  indicesDataSize  = sizeof( uint16_t ) * ( m_IndexBufferSize = FaceIndicesCount * chunk.m_VisibleFacesCount );
     auto&       api              = MainApplication::GetInstance( ).GetVulkanAPI( );
     const auto& noiseGenerator   = MinecraftServer::GetInstance( ).GetWorld( ).GetHeightNoise( );
 
+    std::unique_ptr<DataType::ColoredVertex[]> chunkVertices = std::make_unique<DataType::ColoredVertex[]>( FaceVerticesCount * chunk.m_VisibleFacesCount );
+    std::unique_ptr<uint16_t[]>                chunkIndices  = std::make_unique<uint16_t[]>( m_IndexBufferSize );
 
-    std::unique_ptr<DataType::ColoredVertex[]> chunkVertices = std::make_unique<DataType::ColoredVertex[]>( BlockVertices.size( ) << ( SectionBinaryOffsetLength * 2 ) );
-
-    m_IndexBufferSize                        = blockIndices.size( ) << ( SectionBinaryOffsetLength * 2 );
-    std::unique_ptr<uint16_t[]> chunkIndices = std::make_unique<uint16_t[]>( m_IndexBufferSize );
-
-    const auto blockVerticesSize = BlockVertices.size( );
-    const auto blockIndicesSize  = blockIndices.size( );
-    for ( int i = 0; i < SectionUnitLength; ++i )
-    {
-        for ( int j = 0; j < SectionUnitLength; ++j )
+    auto AddFace = [ faceAdded = 0, chunkVerticesPtr = chunkVertices.get( ), chunkIndicesPtr = chunkIndices.get( ) ]( const std::array<DataType::ColoredVertex, FaceVerticesCount>& vertexArray, const glm::vec3& offset ) mutable {
+        for ( int k = 0; k < FaceVerticesCount; ++k, ++chunkVerticesPtr )
         {
-            const auto height                     = noiseGenerator->GetNoiseInt( chunkX + i, chunkZ + j ) * 10;
-            const auto blockCoordinateIndexVertex = ( i * SectionUnitLength + j ) * blockVerticesSize;
-            for ( int k = 0; k < blockVerticesSize; ++k )
-            {
-                assert( blockCoordinateIndexVertex + k < ( BlockVertices.size( ) << ( SectionBinaryOffsetLength * 2 ) ) );
-                chunkVertices[ blockCoordinateIndexVertex + k ] = BlockVertices[ k ];
-                chunkVertices[ blockCoordinateIndexVertex + k ].pos += glm::vec3( chunkX + i, height, chunkZ + j );
-            }
+            *chunkVerticesPtr = vertexArray[ k ];
+            chunkVerticesPtr->pos += offset;
+        }
 
-            const auto blockCoordinateIndexIndices = ( i * SectionUnitLength + j ) * blockIndicesSize;
-            for ( int k = 0; k < blockIndicesSize; ++k )
+        for ( int k = 0; k < FaceIndicesCount; ++k, ++chunkIndicesPtr )
+        {
+            *chunkIndicesPtr = blockIndices[ k ] + faceAdded * FaceVerticesCount;
+        }
+
+        ++faceAdded;
+    };
+
+    for ( int y = 0, blockIndex = 0; y < chunk.m_SectionHeight; ++y )
+    {
+        for ( int z = 0; z < SectionUnitLength; ++z )
+        {
+            for ( int x = 0; x < SectionUnitLength; ++x, ++blockIndex )
             {
-                assert( blockCoordinateIndexIndices + k < ( blockIndices.size( ) << ( SectionBinaryOffsetLength * 2 ) ) );
-                chunkIndices[ blockCoordinateIndexIndices + k ] = blockIndices[ k ] + ( i * SectionUnitLength + j ) * blockVerticesSize;
+                if ( const auto visibleFace = chunk.m_BlockFaces[ blockIndex ] )
+                {
+                    const auto offset = glm::vec3( chunkX + x, y, chunkZ + z );
+                    if ( visibleFace & DirFrontBit )
+                        AddFace( FrontBlockVertices, offset );
+                    if ( visibleFace & DirBackBit )
+                        AddFace( BackBlockVertices, offset );
+                    if ( visibleFace & DirRightBit )
+                        AddFace( RightBlockVertices, offset );
+                    if ( visibleFace & DirLeftBit )
+                        AddFace( LeftBlockVertices, offset );
+                    if ( visibleFace & DirUpBit )
+                        AddFace( UpBlockVertices, offset );
+                    if ( visibleFace & DirDownBit )
+                        AddFace( DownBlockVertices, offset );
+                }
             }
         }
     }
@@ -84,7 +145,4 @@ ChunkCache::ResetLoad( )
         stagingBuffer.writeBuffer( chunkIndices.get( ), indicesDataSize, api );
         m_IndexBuffer.CopyFromBuffer( stagingBuffer, bufferRegion, api );
     }
-
-    // using namespace std::chrono_literals;
-    // std::this_thread::sleep_for(1s);
 }
