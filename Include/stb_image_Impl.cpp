ac41570f13131342d2ff635b90e52bd492df3dfc
EMCJava <loyusum246807952@gmail.com>
2022-07-09 09:39:45 +0800

    Added std image library

---
+++ Include/stb_image_Impl.cpp
@@ -0,0 +1,11 @@
+//
+// Created by loys on 7/7/22.
+//
+
+#ifndef MINECRAFT_VK_STB_IMAGE_IMPL_CPP
+#define MINECRAFT_VK_STB_IMAGE_IMPL_CPP
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#endif   // MINECRAFT_VK_STB_IMAGE_IMPL_CPP
