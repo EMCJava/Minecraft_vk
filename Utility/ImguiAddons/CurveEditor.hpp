d9dbb6922ab88ceeb340ebaf3a9d8d62d72f2b1a
EMCJava <loyusum246807952@gmail.com>
2022-07-10 19:26:29 +0800

    Added curve setter and serializer

--- Utility/ImguiAddons/CurveEditor.hpp
+++ Utility/ImguiAddons/CurveEditor.hpp
@@ -6,6 +6,8 @@
 #define MINECRAFT_VK_UTILITY_IMGUIADDONS_CURVEEDITOR_HPP
 
 #include <Include/imgui_include.hpp>
+
+#include <iostream>
 #include <vector>
 
 namespace ImGuiAddons
@@ -13,10 +15,12 @@ namespace ImGuiAddons
 
 class CurveEditor
 {
+public:
     using Point = ImPlotPoint;
 
+private:
     std::vector<Point> m_Points;
-    int                draggingBeginIndex = -1, draggingCurrentIndex { };
+    int draggingBeginIndex = -1, draggingCurrentIndex { };
 
 public:
     CurveEditor( )
@@ -36,7 +40,29 @@ public:
     bool Render( );
 
     double Sample( float X );
+
+    void SetCurve( std::vector<Point>&& points )
+    {
+        m_Points = std::move( points );
+    }
+
+    friend inline std::ostream& operator<<( std::ostream& os, const CurveEditor& ce );
 };
+
+inline std::ostream&
+operator<<( std::ostream& os, const CurveEditor& ce )
+{
+
+    if ( ce.m_Points.empty( ) ) return os;
+
+    std::cout << "{(" << ce.m_Points[ 0 ].x << " " << ce.m_Points[ 0 ].y << ")";
+    for ( int i = 1; i < ce.m_Points.size( ); i++ )
+    {
+        std::cout << ", (" << ce.m_Points[ i ].x << " " << ce.m_Points[ i ].y << ")";
+    }
+
+    return std::cout << '}' << std::flush;
+}
 
 }   // namespace ImGuiAddons
 
