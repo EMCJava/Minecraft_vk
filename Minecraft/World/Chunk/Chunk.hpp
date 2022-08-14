3a3072b77c0c30e10b87d3a3bd4bf40bf576937a
EMCJava <loyusum246807952@gmail.com>
2022-08-15 01:28:46 +0800

    Added aabb functions

--- Minecraft/World/Chunk/Chunk.hpp
+++ Minecraft/World/Chunk/Chunk.hpp
@@ -128,7 +128,7 @@ public:
      * Stats
      *
      * */
-    CoordinateType GetHeight( uint32_t index );
+    CoordinateType GetHeight( uint32_t index, HeightMapStatus status = eFullHeight );
 
     [[nodiscard]] bool IsChunkStatusAtLeast( ChunkStatus status ) const { return m_Status >= status; }
 
@@ -140,6 +140,7 @@ public:
     [[nodiscard]] const Block* CheckBlock( const BlockCoordinate& blockCoordinate ) const;
     [[nodiscard]] Block*       GetBlock( const BlockCoordinate& blockCoordinate );
 
+    bool SetBlockAtWorldCoordinate( const BlockCoordinate& blockCoordinate, const Block& block );
     bool SetBlock( const BlockCoordinate& blockCoordinate, const Block& block );
     bool SetBlock( const uint32_t& blockIndex, const Block& block );
 
@@ -147,7 +148,16 @@ public:
 
     inline auto GetChunkNoise( ) { return m_ChunkNoise; }
 
-    inline const ChunkCoordinate& GetCoordinate( ) { return m_Coordinate; }
+    inline auto&                  GetWorld( ) { return m_World; }
+    inline BlockCoordinate        WorldToChunkRelativeCoordinate( const BlockCoordinate& blockCoordinate ) { return blockCoordinate - m_WorldCoordinate; }
+    inline const ChunkCoordinate& GetChunkCoordinate( ) { return m_Coordinate; }
+    inline const ChunkCoordinate& GetWorldCoordinate( ) { return m_WorldCoordinate; }
+    inline bool                   IsPointInside( const BlockCoordinate& blockCoordinate ) { return IsPointInsideVertically( blockCoordinate ) && IsPointInsideHorizontally( blockCoordinate ); }
+    inline bool                   IsPointInsideVertically( const BlockCoordinate& blockCoordinate ) const { return GetMinecraftY( blockCoordinate ) < ChunkMaxHeight && GetMinecraftY( blockCoordinate ) >= 0; }
+    inline bool                   IsPointInsideHorizontally( const BlockCoordinate& blockCoordinate ) const
+    {
+        return GetMinecraftX( blockCoordinate ) >= GetMinecraftX( m_WorldCoordinate ) && GetMinecraftX( blockCoordinate ) < GetMinecraftX( m_WorldCoordinate ) + SectionUnitLength && GetMinecraftZ( blockCoordinate ) >= GetMinecraftZ( m_WorldCoordinate ) && GetMinecraftZ( blockCoordinate ) < GetMinecraftZ( m_WorldCoordinate ) + SectionUnitLength;
+    }
 
     [[nodiscard]] inline CoordinateType ManhattanDistance( const Chunk& other ) const
     {
