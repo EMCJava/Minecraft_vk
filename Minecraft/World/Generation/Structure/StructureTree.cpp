672a412ce6ae941d43d802b72656d446f12510ad
EMCJava <loyusum246807952@gmail.com>
2022-08-15 00:28:03 +0800

    Structure generation avoid setting block outside chunk and leave

--- Minecraft/World/Generation/Structure/StructureTree.cpp
+++ Minecraft/World/Generation/Structure/StructureTree.cpp
@@ -5,17 +5,71 @@
 #include "StructureTree.hpp"
 
 #include "Minecraft/World/Chunk/Chunk.hpp"
+#include "Minecraft/World/MinecraftWorld.hpp"
+#include "Utility/Logger.hpp"
+
+namespace
+{
+
+auto
+ToHorizontalIndex( const auto& coordinate )
+{
+    return ScaleToSecond<1, SectionUnitLength>( GetMinecraftZ( coordinate ) ) + GetMinecraftX( coordinate );
+}
+};   // namespace
 
 void
 StructureTree::Generate( Chunk& chunk )
 {
-    auto startingIndex = ScaleToSecond<1, SectionUnitLength>( GetMinecraftZ( m_StartingPosition ) ) + GetMinecraftX( m_StartingPosition );
-    auto heightAtPoint = chunk.GetHeight( startingIndex ) + 1;
-    auto treeHeight    = std::min( ChunkMaxHeight - heightAtPoint, 7 );
-    for ( int i = 0; i < treeHeight; ++i )
+    auto           startingCoordinate = chunk.WorldToChunkRelativeCoordinate( m_StartingPosition );
+    auto           startingIndex      = ToHorizontalIndex( startingCoordinate );
+    CoordinateType heightAtPoint, treeHeight;
+    MinecraftNoise chunkNoise;
+
+    if ( chunk.IsPointInsideHorizontally( m_StartingPosition ) )
     {
-        chunk.SetBlock( startingIndex + ScaleToSecond<1, SectionSurfaceSize>( heightAtPoint + i ), BlockID::AcaciaLog );
+        heightAtPoint = chunk.GetHeight( startingIndex, eNoiseHeight ) + 1;
+        treeHeight    = std::min( ChunkMaxHeight - heightAtPoint, 7 );
+        chunkNoise    = chunk.GetChunkNoise( );
+
+    } else
+    {
+        assert( GetMinecraftY( m_StartingPosition ) == 0 );
+        const auto originalChunkCoordinate = m_StartingPosition >> SectionUnitLengthBinaryOffset;
+        // const
+        auto originalChunk           = chunk.GetWorld( )->GetChunkCache( originalChunkCoordinate );
+        const auto relativeCoordinate      = originalChunk->WorldToChunkRelativeCoordinate( m_StartingPosition );
+        if ( GetMinecraftX( relativeCoordinate ) < 0 || GetMinecraftX( relativeCoordinate ) >= SectionUnitLength || GetMinecraftZ( relativeCoordinate ) < 0 || GetMinecraftZ( relativeCoordinate ) >= SectionUnitLength )
+            originalChunk = chunk.GetWorld( )->GetChunkCache( originalChunkCoordinate );
+        const auto horizontalIndex = ToHorizontalIndex( relativeCoordinate );
+        heightAtPoint              = originalChunk->GetHeight( horizontalIndex, eNoiseHeight ) + 1;
+        treeHeight                 = std::min( ChunkMaxHeight - heightAtPoint, 7 );
+        chunkNoise                 = originalChunk->GetChunkNoise( );
     }
+
+    // trunk
+    if ( chunk.IsPointInsideHorizontally( m_StartingPosition ) )
+    {
+
+        for ( int i = 0; i < treeHeight; ++i )
+        {
+            chunk.SetBlock( startingIndex + ScaleToSecond<1, SectionSurfaceSize>( heightAtPoint + i ), BlockID::AcaciaLog );
+        }
+    }
+
+    auto leafOrigin             = m_StartingPosition;
+    GetMinecraftY( leafOrigin ) = heightAtPoint + treeHeight;
+
+    for ( int x = -4; x <= 4; ++x )
+        for ( int y = -4; y <= 4; ++y )
+            for ( int z = -4; z <= 4; ++z )
+            {
+                const auto leafDensity = std::abs( x ) + std::abs( y ) + std::abs( z );
+                if ( chunkNoise.NextUint64( ) % 8 >= leafDensity )
+                {
+                    chunk.SetBlockAtWorldCoordinate( leafOrigin + MakeMinecraftCoordinate( x, y, z ), BlockID::AzaleaLeaves );
+                }
+            }
 }
 
 bool
@@ -28,13 +82,16 @@ StructureTree::TryGenerate( Chunk& chunk, std::vector<std::shared_ptr<Structure>
     for ( int k = 0; k < SectionUnitLength; ++k )
         for ( int j = 0; j < SectionUnitLength; ++j, ++horizontalMapIndex )
         {
-            if ( chunkNoise.NextUint64( ) % 80 == 0 )
+            if ( chunkNoise.NextUint64( ) % ( 16 * 16 * 10 ) == 0 )
             {
-                generated = true;
+                generated          = true;
+                auto chunkPosition = chunk.GetWorldCoordinate( );
+                auto newStructure  = std::make_shared<StructureTree>( );
 
-                auto newStructure                                 = std::make_shared<StructureTree>( );
-                GetMinecraftX( newStructure->m_StartingPosition ) = j;
-                GetMinecraftZ( newStructure->m_StartingPosition ) = k;
+                GetMinecraftX( newStructure->m_StartingPosition ) = GetMinecraftX( chunkPosition ) += j;
+                GetMinecraftZ( newStructure->m_StartingPosition ) = GetMinecraftZ( chunkPosition ) += j;
+
+                newStructure->AddPiece( { GetMinecraftX( chunkPosition ) - 4.0f, GetMinecraftZ( chunkPosition ) - 4.0f, GetMinecraftX( chunkPosition ) + 4.0f, GetMinecraftZ( chunkPosition ) + 4.0f } );
 
                 structureList.emplace_back( std::move( newStructure ) );
             }
